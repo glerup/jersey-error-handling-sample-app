5796146d9b28758558c6e777e96707507a12488c
Rune Glerup <rune.glerup@gmail.com>
2014-02-24 11:41:23 +0100

    Experiment with workaround by using selectVariant()

--- src/org/example/errorhandling/Greeter.java
+++ src/org/example/errorhandling/Greeter.java
@@ -1,13 +1,18 @@
 package org.example.errorhandling;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.Variant;
 
 import org.example.errorhandling.repr.Error;
 import org.example.errorhandling.repr.Greeting;
@@ -17,11 +22,13 @@ import org.example.errorhandling.repr.Greeting;
 public class Greeter {
 	private final String name;
 
-	public Greeter(@PathParam("name") String name) {
+	public Greeter(@PathParam("name") String name, @Context Request request) {
 		if ("Vader".equals(name)) {
 			Error error = new Error();
 			error.message = "Illegal name";
-			Response errorResponse = Response.status(Status.BAD_REQUEST).entity(error).build();
+			List<Variant> variants = Variant.mediaTypes(MediaType.APPLICATION_JSON_TYPE, MediaType.APPLICATION_XML_TYPE).add().build();
+			Variant variant = request.selectVariant(variants);
+			Response errorResponse = Response.status(Status.BAD_REQUEST).entity(error).variant(variant).build();
 			throw new WebApplicationException(errorResponse);
 		} else {
 			this.name = name;
